3780734c9426c7599481b0a1b4c0edbe41bb9f0e
reimondo <reimondo.zero@gmail.com>
2015-03-16 21:53:50 +0800

    First init

---
+++ JS/JScript.js
@@ -0,0 +1,10 @@
+﻿
+
+    function iFrameHeight() {
+        var ifm= document.getElementById("iframepage");
+        var subWeb = document.frames ? document.frames["iframepage"].document :
+ifm.contentDocument;
+            if(ifm != null && subWeb != null) {
+            ifm.height = subWeb.body.scrollHeight;
+            }
+    }
