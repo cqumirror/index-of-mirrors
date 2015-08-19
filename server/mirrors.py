60d4cb6b81fe5140a66bed3d86c0884ef75cf387
Zhiqiang He <reimondo.zero@gmail.com>
2015-08-19 18:25:48 +0800

    Ignore some info and correct some bad tips

--- server/mirrors.py
+++ server/mirrors.py
@@ -191,8 +191,9 @@ def get_mirrors_status():
 @app.route("/api/mirrors/notices")
 def get_mirrors_notices():
     res = {
-        "count": 1,
-        "targets": [{"created_at": "2015-08-21 01:02:00", "notice": "改了下"}]
+        "count": 2,
+        "targets": [{"created_at": "2015-08-21 01:02:00", "notice": "这是第一条公告: 你好,地球"},
+                    {"created_at": "2015-08-21 11:02:00", "notice": "这是第二条公告: Beta 测试"}]
     }
     return jsonify(res)
 
